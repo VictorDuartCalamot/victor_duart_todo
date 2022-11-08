d7a799e64b394e314fc28c65d3c8cdc2ed9eced4
VictorDuartCalamot <115024032+VictorDuartCalamot@users.noreply.github.com>
2022-11-08 19:19:21 +0100

    Add files via upload

---
+++ lib/Controller.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import 'Model.dart';
+import 'main.dart';
+
+
+/*
+
+class TodoController {
+
+
+}
+
+
+void _addTodoItem(String title) {
+  setState(() {
+    _todoList.add(title);
+  });
+  _textFieldController.clear();
+}
+*/
---
+++ lib/Model.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+
+
+
+
+/*
+class Model {
+  final List<String> _todoList = <String>[];
+  factory Model(){
+    return _getItems();
+  }
+
+
+  List<Widget> _getItems() {
+    final List<Widget> _todoWidgets = <Widget>[];
+    for (String title in _todoList) {
+      _todoWidgets.add(_buildTodoItem(title));
+    }
+    return _todoWidgets;
+  }
+
+
+
+
+
+}*/
+
